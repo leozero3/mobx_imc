30b4221f782d2f1e81cb7f58ec5bb49819686874
leo <realleo2009@hotmail.com>
2022-04-05 17:47:53 -0300

    mobx imc

--- lib/contador_codegen/contador_codegen_controller.dart
+++ lib/contador_codegen/contador_codegen_controller.dart
@@ -5,5 +5,7 @@ part 'contador_codegen_controller.g.dart';
 class ContadorCodeGenController = _ContadorCodeGenControllerBase with _$ContadorCodeGenController;
 
 abstract class _ContadorCodeGenControllerBase with Store{
+  @observable
+  int counter = 0;
 
 }
